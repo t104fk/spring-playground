b62c7053f0f7533cfdfbf8d6628084af2644928d
takasing <takaty2011@gmail.com>
2015-01-23 11:01:44 +0900

    throw api.

--- src/main/java/net/takasing/controller/TestController.java
+++ src/main/java/net/takasing/controller/TestController.java
@@ -2,16 +2,18 @@ package net.takasing.controller;
 
 import net.takasing.command.TestCommand;
 import net.takasing.model.User;
+import net.takasing.service.HttpService;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 
 /**
  * @author toyofuku_takashi
@@ -19,6 +21,8 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class TestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+    @Autowired
+    private HttpService httpService;
     @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
     public String get() {
         return "test";
@@ -32,16 +36,41 @@ public class TestController {
         command.setUser(user);
         return "templateWithModel";
     }
+    @RequestMapping(value = {"/templateWithParam"}, method = RequestMethod.GET)
+    public String getTemplateWithParam(@ModelAttribute("command") TestCommand command,
+                                       @RequestParam("username") String username,
+                                       @RequestParam("mailAddress") String mailAddress) {
+        command.setName("test controller.");
+        User user = new User();
+        user.setUsername(username);
+        user.setMailAddress(mailAddress);
+        command.setUser(user);
+        return "templateWithModel";
+    }
     @RequestMapping(value = {"/api/input"}, method = RequestMethod.GET)
     public String getFromApi() {
         return "form";
     }
-    @RequestMapping(value = {"/api/input"}, method = RequestMethod.POST)
-    public String postAndGetFromApi(@RequestBody String postPayload,
-                                  HttpServletRequest request,
-                                  HttpServletResponse response) {
+    @RequestMapping(value = {"/simple/input"}, method = RequestMethod.POST)
+    public String post(@RequestBody String postPayload,
+                                  HttpServletRequest request) {
         LOGGER.info("@RequestBody's String payload=" + postPayload);
         request.setAttribute("payload", postPayload);
         return "result";
+    }
+    @RequestMapping(value = {"/api/input"}, method = RequestMethod.POST)
+    public void postAndGetFromApi(@RequestParam("username") String username,
+                                    @RequestParam("mailAddress") String mailAddress,
+                                  HttpServletResponse response) throws Exception {
+        URI uri = new URIBuilder()
+                .setScheme("http")
+                .setHost("localhost")
+                .setPort(8080)
+                .setPath("/templateWithParam")
+                .addParameter("username", username)
+                .addParameter("mailAddress", mailAddress)
+                .build();
+        String entity = httpService.request(new HttpGet(uri));
+        httpService.response(response, entity);
     }
 }
--- src/main/webapp/WEB-INF/templates/form.ftl
+++ src/main/webapp/WEB-INF/templates/form.ftl
@@ -2,6 +2,14 @@
 <meta charset="utf-8">
 <title>response from api.</title>
 <article>
+<h1>forms</h1>
+<h2>simple post</h2>
+<form name="" action="/simple/input" method="post">
+<input type="text" name="username">
+<input type="text" name="mailAddress">
+<input type="submit" value="post">
+</form>
+<h2>through api post</h2>
 <form name="" action="/api/input" method="post">
 <input type="text" name="username">
 <input type="text" name="mailAddress">
