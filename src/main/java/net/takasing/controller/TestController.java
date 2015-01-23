d81d9469597a4a51d729dbbd101a24f6cb9242b2
takasing <takaty2011@gmail.com>
2015-01-23 10:20:42 +0900

    post, @RequestBody

--- src/main/java/net/takasing/controller/TestController.java
+++ src/main/java/net/takasing/controller/TestController.java
@@ -2,16 +2,23 @@ package net.takasing.controller;
 
 import net.takasing.command.TestCommand;
 import net.takasing.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author toyofuku_takashi
  */
 @Controller
 public class TestController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
     @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
     public String get() {
         return "test";
@@ -24,5 +31,17 @@ public class TestController {
         user.setMailAddress("toyo@mail.com");
         command.setUser(user);
         return "templateWithModel";
+    }
+    @RequestMapping(value = {"/api/input"}, method = RequestMethod.GET)
+    public String getFromApi() {
+        return "form";
+    }
+    @RequestMapping(value = {"/api/input"}, method = RequestMethod.POST)
+    public String postAndGetFromApi(@RequestBody String postPayload,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response) {
+        LOGGER.info("@RequestBody's String payload=" + postPayload);
+        request.setAttribute("payload", postPayload);
+        return "result";
     }
 }
---
+++ src/main/webapp/WEB-INF/templates/form.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>response from api.</title>
+<article>
+<form name="" action="/api/input" method="post">
+<input type="text" name="username">
+<input type="text" name="mailAddress">
+<input type="submit" value="post">
+</form>
+</article>
